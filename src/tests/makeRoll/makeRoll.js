cab25aa3e8517b99680881cc74eb38afe8bed48a
Nic Bradley <nic.m.bradley@gmail.com>
2021-03-13 19:25:34 -0800

    Initializing tests for new constructRoll function

---
+++ src/tests/makeRoll/makeRoll.js
@@ -0,0 +1,101 @@
+const makeRoll = (roll, target, advantage = false, difficulty = false) => {
+    const roll_array = [];
+    const full_target = [
+        `@{${target}}`,
+        `+@{difficulty_query}`,
+        `+@{roll_query}`
+    ].join("");
+    const advantage_target = [
+        `@{${target}}`,
+        `+@{difficulty_query}`,
+        `+@{roll_query}`,
+        `+(@{advantage}*10)`
+    ].join("");
+
+    roll_array.push(`@{roll_whisper}`);
+    roll_array.push(`&{template:wfrp}`);
+    roll_array.push(`{{name=@{character_name}}}`);
+
+    if (target) {
+        roll_array.push(`{{roll=[[1d100cs<@{roll_cs}cf>@{roll_cf}]]}}`);
+
+        if (difficulty === "combat") roll_array.push(`{{difficulty=[[@{difficulty_query_combat}]]}}`);
+        else roll_array.push(`{{difficulty=[[@{difficulty_query}]]}}`);
+
+        roll_array.push(`{{bonus=[[@{roll_query}]]}}`);
+        roll_array.push(`@{roll_slbonus}`);
+    }
+    if (target && !advantage) {
+        roll_array.push(`{{target=[[${full_target}]]}}`);
+    } else if (target && advantage) {
+        roll_array.push(`{{target=[[${advantage_target}]]}}`);
+        roll_array.push(`{{roll_advantage=[[(@{advantage}*10)]]}}`);
+    }
+
+    Object.entries(roll).forEach(([key, val]) => roll_array.push(`{{${key}=${val}}}`));
+
+    return roll_array.join(" ");
+}
+
+const throwError = () => { throw new Error("error") }
+
+const constructRoll = (props) => {
+    // Throw error if no props.
+    if (!props) throw new Error("No props provided to constructRoll");
+
+    // Destructure props
+    const { name, type, notes = "", attr, advantage = false, category, children } = props;
+
+    // Find any missing fields
+    const missing_fields = [];
+
+    if (!name) missing_fields.push("name")
+    if (!type) missing_fields.push("type")
+    if (!category) missing_fields.push("category")
+
+    // Reject if fields are missing
+    if (missing_fields.length > 0) throw new Error(`Missing ${missing_fields.join(", ")} from parameters.`)
+
+    // Define some useful variables
+    const difficulty = (category === ("combat" || "magic")) ? `{{difficulty=[[@{difficulty_query_combat}]]}}` :
+        `{{difficulty=[[@{difficulty_query}]]}}`;
+
+    const full_target = [
+        `@{${attr}}`,
+        `+@{difficulty_query}`,
+        `+@{roll_query}`
+    ].join("");
+
+    const advantage_target = [
+        `@{${attr}}`,
+        `+@{difficulty_query}`,
+        `+@{roll_query}`,
+        `+(@{advantage}*10)`
+    ].join("");
+
+    const target = `{{target=[[${(advantage) ? advantage_target : full_target}]]}}`;
+
+    // Init the roll array, set default elements
+    let roll = [
+        `@{roll_whisper}`,
+        `&{template:wfrp}`,
+        `{{name=@{character_name}}}`,
+        `{{roll_type=${type}}}`,
+        `{{roll_name=${name}}}`
+    ];
+
+    if (attr) {
+        roll.push(`{{roll=[[1d100cs<@{roll_cs}cf>@{roll_cf}]]}}`);
+        roll.push(`{{bonus=[[@{roll_query}]]}}`);
+        roll.push(`@{roll_slbonus}`);
+        roll.push(difficulty);
+        roll.push(target);
+        if (advantage) roll.push("{{roll_advantage=[[(@{advantage}*10)]]}}");
+    }
+
+    // Add all remaining children to the roll
+    Object.entries(children).forEach(([key, val]) => roll.push(`{{roll_${key}=${val}}}`));
+
+    // Return the roll array as a joined string
+    return roll.join(" ");
+}
---
+++ src/tests/makeRoll/tests.js
@@ -0,0 +1,223 @@
+let roll = {};
+let parameters = {
+    a: {
+        name: `Accounting`,
+        type: `Skill`,
+        attr: "accounting",
+        advantage: true,
+        category: "skill",
+        children: {
+            notes: `Testing to ensure notes are collected`,
+        }
+    },
+    b: {
+        name: `Longsword`,
+        type: `Basic Attack`,
+        attr: "weapon_attack",
+        advantage: true,
+        category: "combat",
+        children: {
+            target: "one creature",
+            range: "Close",
+            text: `Attack description`,
+            damage: `7 + Opposed SL`,
+            hit_location: "1",
+            crit_location: "[[1d100]]",
+        }
+    },
+    c: {
+        name: `Sixth Sense`,
+        type: `Trait`,
+        category: "trait",
+        children: {
+            text: `The trait description is here.`,
+        }
+    },
+    d: {
+        name: `Dart`,
+        type: `Spell`,
+        attr: `spell_total`,
+        advantage: true,
+        category: `magic`,
+        children: {
+            target: "1 person",
+            duration: "3 days",
+            range: "10 yards",
+            cn: "10",
+            text: "A dart of energy strikes the target.",
+        }
+    }
+};
+
+QUnit.module("constructRoll tests", hooks => {
+
+    QUnit.test("Ensure that the new function produces the same basic result as the first.", assert => {
+
+        // Declare values object
+        const values = {
+            roll_name: parameters.a.name,
+            roll_type: parameters.a.type,
+            roll_notes: parameters.a.children.notes,
+        }
+
+        // Run the two functions with the same data.
+        const result_a = makeRoll(values, parameters.a.attr, parameters.a.advantage, parameters.a.category).split("} ");
+        const result_b = constructRoll(parameters.a).split("} ");
+
+        // Run the test
+        for (const match of result_b) {
+            const is_match = (!result_a.indexOf(match) > -1);
+
+            assert.ok(is_match, `${match} found in new`)
+        }
+    });
+
+    QUnit.test("Ensure that the new function returns a string", assert => {
+
+        // Collect results
+        const result_a = constructRoll(parameters.a);
+
+        // Run the test
+        assert.equal(typeof result_a, "string", "It returns a string.");
+    });
+
+    QUnit.test("Ensure that the new function throws an error with no props, or missing props", assert => {
+
+        // Create malformed data
+        const missing_name = {...parameters.a };
+        const missing_type = {...parameters.a };
+        const missing_attr = {...parameters.a };
+        const missing_category = {...parameters.a };
+
+        delete missing_name.name;
+        delete missing_type.type;
+        delete missing_attr.attr;
+        delete missing_category.category;
+
+        // Run tests
+        assert.throws(() => constructRoll(), new Error("No props provided to constructRoll"), "Throws error for no props");
+        assert.throws(() => constructRoll(missing_name), /name/g, "No name provided to constructRoll");
+        assert.throws(() => constructRoll(missing_type), /type/g, "No type provided to constructRoll");
+        assert.throws(() => constructRoll(missing_category), /category/g, "No category provided to constructRoll");
+    });
+
+    QUnit.test("Ensure that constructRoll correctly handles basic skill rolls", assert => {
+
+        // Collect results 
+        const result_a = constructRoll(parameters.a);
+
+        // List matches
+        const matches = [
+            `@{roll_whisper}`,
+            `&{template:wfrp}`,
+            `{{name=@{character_name}}}`,
+            `{{roll=[[1d100cs<@{roll_cs}cf>@{roll_cf}]]}}`,
+            `{{difficulty=[[@{difficulty_query}]]}}`,
+            `{{bonus=[[@{roll_query}]]}}`,
+            `@{roll_slbonus}`,
+            `{{target=[[@{accounting}+@{difficulty_query}+@{roll_query}+(@{advantage}*10)]]}}`,
+            `{{roll_advantage=[[(@{advantage}*10)]]}}`,
+            `{{roll_name=${parameters.a.name}}}`,
+            `{{roll_type=${parameters.a.type}}}`,
+            `{{roll_notes=${parameters.a.children.notes}}}`,
+        ];
+
+        // Loop through matches, verify content
+        for (match of matches) {
+            const is_match = result_a.indexOf(match) > -1;
+
+            assert.ok(is_match, `${match} was in generated string`);
+        }
+    });
+
+    QUnit.test("Ensure that constructRoll correctly handles weapon rolls", assert => {
+
+        // Collect results
+        const result_b = constructRoll(parameters.b);
+
+        // List matches
+        const matches = [
+            `@{roll_whisper}`,
+            `&{template:wfrp}`,
+            `{{name=@{character_name}}}`,
+            `{{roll=[[1d100cs<@{roll_cs}cf>@{roll_cf}]]}}`,
+            `{{difficulty=[[@{difficulty_query}]]}}`,
+            `{{bonus=[[@{roll_query}]]}}`,
+            `@{roll_slbonus}`,
+            `{{target=[[@{accounting}+@{difficulty_query}+@{roll_query}+(@{advantage}*10)]]}}`,
+            `{{roll_advantage=[[(@{advantage}*10)]]}}`,
+            `{{roll_name=${parameters.a.name}}}`,
+            `{{roll_type=${parameters.a.type}}}`,
+            `{{roll_notes=${parameters.a.children.notes}}}`,
+        ];
+
+        // Loop through matches, verify content
+        for (match of matches) {
+            const is_match = result_b.indexOf(match) > -1;
+
+            assert.ok(is_match, `${match} was in generated string`);
+        }
+
+    })
+
+    QUnit.test("Ensure that constructRoll correctly handles vocalise rolls", assert => {
+
+        // Collect results
+        const result_c = constructRoll(parameters.c);
+
+        // List matches
+        const matches = [
+            `@{roll_whisper}`,
+            `&{template:wfrp}`,
+            `{{name=@{character_name}}}`,
+            `{{roll=[[1d100cs<@{roll_cs}cf>@{roll_cf}]]}}`,
+            `{{difficulty=[[@{difficulty_query}]]}}`,
+            `{{bonus=[[@{roll_query}]]}}`,
+            `@{roll_slbonus}`,
+            `{{target=[[@{accounting}+@{difficulty_query}+@{roll_query}+(@{advantage}*10)]]}}`,
+            `{{roll_advantage=[[(@{advantage}*10)]]}}`,
+            `{{roll_name=${parameters.a.name}}}`,
+            `{{roll_type=${parameters.a.type}}}`,
+            `{{roll_notes=${parameters.a.children.notes}}}`,
+        ];
+
+        // Loop through matches, verify content
+        for (match of matches) {
+            const is_match = result_a.indexOf(match) > -1;
+
+            assert.ok(is_match, `${match} was in generated string`);
+        }
+
+    })
+
+    QUnit.test("Ensure that constructRoll correctly handles magic rolls", assert => {
+
+        // Collect results
+        const result_d = constructRoll(parameters.d);
+
+        // List matches
+        const matches = [
+            `@{roll_whisper}`,
+            `&{template:wfrp}`,
+            `{{name=@{character_name}}}`,
+            `{{roll=[[1d100cs<@{roll_cs}cf>@{roll_cf}]]}}`,
+            `{{difficulty=[[@{difficulty_query}]]}}`,
+            `{{bonus=[[@{roll_query}]]}}`,
+            `@{roll_slbonus}`,
+            `{{target=[[@{accounting}+@{difficulty_query}+@{roll_query}+(@{advantage}*10)]]}}`,
+            `{{roll_advantage=[[(@{advantage}*10)]]}}`,
+            `{{roll_name=${parameters.a.name}}}`,
+            `{{roll_type=${parameters.a.type}}}`,
+            `{{roll_notes=${parameters.a.children.notes}}}`,
+        ];
+
+        // Loop through matches, verify content
+        for (match of matches) {
+            const is_match = result_a.indexOf(match) > -1;
+
+            assert.ok(is_match, `${match} was in generated string`);
+        }
+
+    })
+
+})
